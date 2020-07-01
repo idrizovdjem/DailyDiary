bb0be45747ae0961d93f9b8a19bedd2dbd87da5c
idrizovdjem <idrizovdjem@gmail.com>
2020-07-01 19:06:44 +0300

    Created login page style

--- diary-front-end/src/App.js
+++ diary-front-end/src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './App.css';
+import Login from './Components/Login/Login';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 'login'
+    };
+
+    this.getActivePage = this.getActivePage.bind(this);
+  }
+
+  getActivePage() {
+    switch(this.state.activePage) {
+      case 'login': return <Login />;
+      default: throw 'Invalid active page state';
+    }
+  }
+
+  render() {
+    let renderedPage = this.getActivePage();
+
+    return(
+      <div className="App">
+        {renderedPage}
+      </div>
+    );
+  }
 }
 
 export default App;
