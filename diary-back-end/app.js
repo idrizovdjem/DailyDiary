e703eacb197b1f7b5dd88d9a76b4869a112b6c96
idrizovdjem <idrizovdjem@gmail.com>
2020-07-04 08:20:07 +0300

    Added basic functionality to the back-end server

--- diary-back-end/app.js
+++ diary-back-end/app.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const appRouter = require('./appRouter.js');
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', appRouter);
 
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
